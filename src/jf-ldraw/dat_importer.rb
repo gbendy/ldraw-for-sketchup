c3eacbfb522b2c6061f5fa8749d107584d8d9e0b
Jim Foltz <jim.foltz@gmail.com>
2012-11-24 09:42:19 -0500

    Moved menu into loader.rb

--- src/jf-ldraw/dat_importer.rb
+++ src/jf-ldraw/dat_importer.rb
@@ -2,14 +2,6 @@
 #
 require 'sketchup'
 
-menu = UI.menu('File')#.add_submenu('LDraw Import')
-cmd  = UI::Command.new("Import LDraw by PN") {
-  JF::LDraw.import_part_by_number
-}
-menu.add_item(cmd)
-#cmd = UI::Command.new("Import File") { JF::LDraw.get_file }
-#menu.add_item(cmd)
-
 
 module JF
     module LDraw
