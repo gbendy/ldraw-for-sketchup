53ccebe34402a74b329a47028ff1d993f36c5a3b
Jim Foltz <jim.foltz@gmail.com>
2012-11-24 09:03:42 -0500

    Made into SketchupExtension

---
+++ src/jf-ldraw-ext.rb
@@ -0,0 +1,17 @@
+require 'sketchup'
+require 'extensions'
+
+module JF
+  module LDraw
+    extension = SketchupExtension.new(
+      'LDraw Tools',
+      'jf-ldraw/loader.rb'
+    )
+    extension.description = 'LDraw Tools for SketchUp'
+    extension.version     = '0.1'
+    extension.copyright   = '2012 jim.foltz@gmail.com'
+    extension.ceator      = 'Jim Foltz'
+
+    Sketchup.register_extension(extension, true)
+  end
+end
