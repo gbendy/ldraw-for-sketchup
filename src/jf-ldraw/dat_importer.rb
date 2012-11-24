91b0f1273be6465ded81b48093d8be7ffeaae246
Jim Foltz <jim.foltz@gmail.com>
2012-11-24 13:00:08 -0500

    cleanup

--- src/jf-ldraw/dat_importer.rb
+++ src/jf-ldraw/dat_importer.rb
@@ -8,6 +8,7 @@ module JF
 
     CMD_COMMENT = 0
     CMD_FILE    = 1
+    CMD_LINE    = 2
     CMD_TRI     = 3
     CMD_QUAD    = 4
 
@@ -20,7 +21,7 @@ module JF
       init()
       @last_pn = "3001" unless @last_pn
       ret = UI.inputbox(["Part No"], [@last_pn], "Import DAT")
-      unless ret
+      if ret == false
         Sketchup.status_text = "Canceled."
         return
       end
@@ -30,13 +31,17 @@ module JF
       return ret[0]
     end
 
-    def self.get_file
+    
+    # Allow user to browse for a file.
+    # @return String<filepath> or nil
+    def self.ui_get_file
       init()
       file = UI.openpanel("Model", @ldrawdir, "*.dat")
       return unless file
       go(File.basename(file))
     end
 
+    # @param [String] pn - LDraw part number including .dat extention
     def self.go(pn)
       init()
       file = rel_path_to(pn)
@@ -44,39 +49,37 @@ module JF
         UI.messagebox("No part: #{pn}")
         return
       end
-      name = File.basename(file, '')
       Sketchup.active_model.definitions.purge_unused
       f = File.new(File.join(@ldrawdir, file))
       first_line = f.readline
-      first_line = first_line.gsub(/^0/, '').strip
       f.close
-      cdef  = Sketchup.active_model.definitions.add(File.basename(name, '.dat'))
+      first_line = first_line.gsub(/^0/, '').strip
+      name = File.basename(file, '.dat')
+      cdef = Sketchup.active_model.definitions.add(name)
       cdef.description = first_line
       entities = cdef.entities
+
       Sketchup.active_model.start_operation "Import", true
       tr = Geom::Transformation.new
       read_file(file, entities, tr)
       ins = Sketchup.active_model.entities.add_instance(cdef, tr)
       Sketchup.active_model.commit_operation
+
     end
 
 
-
-    def self.add_def(name, desc = "")
+    def self.get_or_add_definition(name, desc = "")
       name = name.split('.')[0]
       raise "#{__LINE__} - bad name." if name.empty? or name.nil?
       if((cdef = Sketchup.active_model.definitions[name]))
         return cdef
       else
         cdef = Sketchup.active_model.definitions.add(name) 
-        pt = cdef.entities.add_cpoint([0, 0, 0])
-        pt.layer = @const_layer
         return cdef
       end
     end
 
     def self.read_file(file, container, matrix)
-      #p file
       file = File.join(@ldrawdir, file)
       lines = IO.readlines(file)
       lines.each_with_index do |line, i|
@@ -86,15 +89,17 @@ module JF
         cmd = cmd.to_i
         color = ary.shift
         case cmd
+        when CMD_LINE
+          # Do nothing
         when CMD_FILE # File
           name = (ary.pop).downcase
-          raise "Bad array line #{i} #{File.basename(file)}" if ary.length != 12
-          part_m = ary_to_trans(ary)
-          next if name[/edge/i]
-          part_def = add_def(name)
+          raise "Bad array #{File.basename(file)}:#{i}" if ary.length != 12
+          #next if name[/edge/i] # No need to add edges
+          part_def = get_or_add_definition(name)
           if part_def.entities.length <= 1
             read_file(rel_path_to(name), part_def.entities, matrix)
           end
+          part_m = ary_to_trans(ary)
           part = container.add_instance(part_def, part_m)
         when CMD_TRI
           ary.map!{|e| e.to_f }
@@ -108,9 +113,9 @@ module JF
           end
         when CMD_QUAD
           ary.map!{|e| e.to_f }
-          pts = [ ary[0, 3], ary[3, 3], ary[6, 3], ary[9, 3] ]
-          pts.map!{|e| Geom::Point3d.new(e)}
-          pts.map!{|e| e.transform!(matrix)}
+          pts = [ ary[0..2], ary[3..5], ary[6..8], ary[9..11] ]
+          #pts.map!{|e| Geom::Point3d.new(e)}
+          #pts.map!{|e| e.transform!(matrix)}
           p1, p2, p3, p4 = pts
           if swap_needed(pts)
             if( !swap_needed([p1, p2, p4, p3]) )
@@ -130,6 +135,7 @@ module JF
       end
     end
 
+    # Finds part.dat file relative to the @ldrawdir folder.
     def self.rel_path_to(name)
       name += ".dat" unless(name.split('.')[1] == "dat")
       if (File.exist?( path = File.join(@ldrawdir, "parts", name)))
@@ -151,15 +157,15 @@ module JF
       r4 = [x, y, z, 1.0]
       na = r1 + r2 + r3 + r4
       na.map!{|e| e.to_f}
-      t = Geom::Transformation.new(na)
+      Geom::Transformation.new(na)
     end
 
     def self.swap_needed(ary)
       p1, p2, p3, p4 = ary
-      n1 = (p1.vector_to p4) * (p1.vector_to p2).normalize
-      n2 = (p2.vector_to p1) * (p2.vector_to p3).normalize
-      n3 = (p3.vector_to p2) * (p3.vector_to p4).normalize
-      n4 = (p4.vector_to p3) * (p4.vector_to p1).normalize
+      n1 = ((p1.vector_to p4) * (p1.vector_to p2)).normalize
+      n2 = ((p2.vector_to p1) * (p2.vector_to p3)).normalize
+      n3 = ((p3.vector_to p2) * (p3.vector_to p4)).normalize
+      n4 = ((p4.vector_to p3) * (p4.vector_to p1)).normalize
       return true if (dot = n1.dot n2) <= 0.0
       return true if (dot = n1.dot n3) <= 0.0
       return true if (dot = n1.dot n4) <= 0.0
