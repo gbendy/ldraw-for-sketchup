4373149ff19a8fbf7263210aa9182050cd11417b
Jim Foltz <jim.foltz@gmail.com>
2012-11-24 10:55:12 -0500

    cleaned up loader.rb

--- src/jf-ldraw/loader.rb
+++ src/jf-ldraw/loader.rb
@@ -1,10 +1,12 @@
+# Loader for LDraw for SketchUp
+#
+
 load "#{File.dirname(__FILE__)}/dat_importer.rb"
 
-menu = UI.menu('File')#.add_submenu('LDraw Import')
-cmd  = UI::Command.new("LDraw Import by PN") {
+menu = UI.menu('File')
+
+cmd  = UI::Command.new("Import LDraw by PN") {
   JF::LDraw.import_part_by_number
 }
-menu.add_item(cmd)
-#cmd = UI::Command.new("Import File") { JF::LDraw.get_file }
-#menu.add_item(cmd)
 
+menu.add_item(cmd)
